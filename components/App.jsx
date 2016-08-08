2ad1e03d2ad36a8f981f2e20deceb6d8c69a46c6
Kenneth Korcal <kakorcal@gmail.com>
2016-08-08 11:14:07 -0700

    adding text input component

--- components/App.jsx
+++ components/App.jsx
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
+import TextInput from './TextInput'
 
 class App extends Component{
   render(){
     return (
       <div>
-        This is a react app!
+        This is a react app~
+        <TextInput/>
       </div>
     );
   }
---
+++ components/TextInput.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+class TextInput extends Component{
+  render(){
+    return <div>This is the TextInput component</div>;
+  }
+}
+
+export default TextInput
