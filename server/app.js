06104cb890a55f541c2c0b1195e3e5d3b7bef2ef
Kenneth Korcal <kakorcal@gmail.com>
2016-08-08 07:50:54 -0700

    adding express server

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 2000;
+
+app.use(express.static('./dist'));
+
+app.use('/', (req, res)=>{
+  res.sendFile(path.resolve('client/index.html'));
+});
+
+app.listen(port, err=>{
+  if(err) throw err;
+  console.log(`Listening to port ${port}`);
+});
