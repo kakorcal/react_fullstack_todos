dec42839a3bd8494638c32092c91ddef9c1af294
Kenneth Korcal <kakorcal@gmail.com>
2016-08-08 07:56:42 -0700

    adding index files

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 2000;
 
+// the built bundle.js is put in here
 app.use(express.static('./dist'));
 
 app.use('/', (req, res)=>{
