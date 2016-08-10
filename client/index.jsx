9cecd50964acedd0f2aeaf1e41dd2ac7477489c2
Kenneth Korcal <kakorcal@gmail.com>
2016-08-10 10:44:28 -0700

    adding root saga

--- client/index.jsx
+++ client/index.jsx
@@ -2,14 +2,14 @@ import 'babel-polyfill'
 import React from 'react'
 import {render} from 'react-dom'
 import App from '../components/App'
-import {loadTodos, watchForLoadTodos} from '../components/sagas/index'
+import rootSaga from '../components/sagas/index'
 import configureStore from '../components/redux/store'
 // wraps the app and grabs state from store to pass it down to components as props
 import {Provider} from 'react-redux'
 
 let initialState = {todos: []};
 let store = configureStore(initialState);
-store.runSaga(watchForLoadTodos);
+store.runSaga(rootSaga);
 
 // configure and create store
 // store = createStore(reducers, initialState)
--- components/sagas/index.js
+++ components/sagas/index.js
@@ -1,17 +1,27 @@
 import {getTodos, postTodo} from '../services/api'
 import {put, take, call, fork, select} from 'redux-saga/effects'
 
-export function* loadTodos(){
+function* loadTodos(){
   const todos = yield getTodos();
   // the put function dispatches actions for you
   yield put({type: 'GET_TODOS', todos});
 }
 
-export function* watchForLoadTodos(){
+function* watchForLoadTodos(){
   while(true){
-    // the take function listens fro
+    // the take function listens for actions of a given type, and when they occur, 
+    // it advances the saga to the next yield.
+    console.log('watch for load todos');
     yield take('GET_TODOS');
+    // the fork function converts watchForLoadTodos into a non-blocking saga, 
+    // meaning other effects can run concurrently.
+    // https://yelouafi.github.io/redux-saga/docs/advanced/NonBlockingCalls.html
     yield fork(loadTodos);
   }
 }
 
+export default function* root(){
+  yield [
+    fork(watchForLoadTodos)
+  ];
+}
