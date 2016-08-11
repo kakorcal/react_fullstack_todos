87288fbb53b8bc34cd732306cd144b4c48f130d9
Kenneth Korcal <kakorcal@gmail.com>
2016-08-11 16:42:07 -0700

    swap let with var for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const path = require('path');
 // for font awesome config: http://www.mattzabriskie.com/blog/font-awesome-webpack#disqus_thread
 // for dev and prod: http://blog.npirotte.be/a-strong-development-webpack-configuration/
 
-let config;
+var config;
 switch(process.env.npm_lifecycle_event){
   case 'dev':
     config = dev;
