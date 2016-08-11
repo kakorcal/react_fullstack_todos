a519fab4198fea7c0f85248b5f3debf58f247251
Kenneth Korcal <kakorcal@gmail.com>
2016-08-11 16:40:24 -0700

    adding production config for knex

--- knexfile.js
+++ knexfile.js
@@ -14,17 +14,20 @@ module.exports = {
       directory: path.resolve('server/db/seeds')
     }
   },
+  
   production: {
-    client: 'pg',
-    debug: true,
-    connection: {
-      database: 'react_todos_app'
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
     },
     migrations: {
-      directory: path.resolve('server/db/migrations')
+      tableName: 'knex_migrations',
+      directory: './server/db/migrations'
     },
     seeds: {
-      directory: path.resolve('server/db/seeds')
+      directory: './server/db/seeds'      
     }
-  },
+  }
 }
