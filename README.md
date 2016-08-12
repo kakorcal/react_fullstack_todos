81373082452bfe26f0593364bfa41baf11b30a29
Kenneth Korcal <kakorcal@gmail.com>
2016-08-11 17:11:47 -0700

    update readme

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 https://react-todos-app.herokuapp.com/
 
+A simple todo app to demonstrate full CRUD capabilities with the following technologies:
+
+* Front-End: React/Redux, Redux-Saga, Axios, Purecss, FontAwesome
+* Back-End: Node/Express, Knex
+* Database: PostgreSQL
+* Tools: Webpack, Webpack-Dev-Middleware
+
 TODOS:
  * ~~Add database for todos~~
  * ~~Use redux-saga to handle requests to database~~
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
 const webpack = require('webpack');
 const dev = require('./webpack.dev.config');
 const prod = require('./webpack.prod.config');
-const parts = require('./webpack.parts');
-const merge = require('webpack-merge');
 const validate = require('webpack-validator');
 const path = require('path');
 
+// TODO: Separate config into parts
+// const parts = require('./webpack.parts');
+// const merge = require('webpack-merge');
+
 // for font awesome config: http://www.mattzabriskie.com/blog/font-awesome-webpack#disqus_thread
 // for dev and prod: http://blog.npirotte.be/a-strong-development-webpack-configuration/
-
 var config;
 switch(process.env.npm_lifecycle_event){
   case 'dev':
